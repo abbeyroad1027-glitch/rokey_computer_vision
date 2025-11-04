9714df2ea01c21abd5576cf3c2e3632e97dba392
abbeyroad1027-glitch <abbeyroad1027@gmail.com>
2025-11-04 23:37:53 +0900

    new file:   Fraud_Transaction_Dection/251104.py

---
+++ Fraud_Transaction_Dection/251104.py
@@ -0,0 +1,40 @@
+#251104.py
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import(
+    confusion_matrix,
+    precision_score,
+    recall_score,
+    f1_score,
+    roc_curve,
+    roc_auc_score
+)
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+# Visualization Settings
+plt.rcParams['font size'] = 12
+plt.rcParams['figure.figsize'] = (10,6)
+plt.rcParams['axes.grid'] = True
+             
+# Data Generation (Class Imbalance)
+# Normal Transaction : 95%
+# Fraud Transaction : 5%
+
+# Let's create imbalanced data (Normal : 95%, Fraud : 5%)
+
+X, y = make_classification(
+    n_samples=10000,
+    n_features=20,
+    n_informative=15,
+    n_redundant=5,
+    n_classes=2,
+    weights=[0.95,0.05],
+    flip_y=0.01,
+    random_state=42
+)
