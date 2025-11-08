368382a03b0c3adda76dfc377a79cfcd5b4713b3
abbeyroad1027-glitch <abbeyroad1027@gmail.com>
2025-11-08 23:17:26 +0900

    AI

---
+++ Introduction_to_AI/intro_ai.py
@@ -0,0 +1,36 @@
+import torch
+import numpy as np
+import torch.optim as optim
+
+def mse(Yp, Y):
+    loss = ((Yp-Y)**2).mean()
+    return loss
+
+Yp_test = torch.tensor([1.0, 2.5, 3.8])
+Y_test = torch.tensor([1.2, 2.0, 4.0])
+
+test_loss = mse(Yp_test, Y_test)
+print(f"{test_loss:.4f}")
+
+print("="*70)
+
+X = torch.tensor([-5., 5., 0., 2., -2.]).float()
+Y = torch.tensor([-6.7, 10.3, -3.3, 5.0, -5.3]).float()
+
+W = torch.tensor(1.0, requires_grad=True).float()
+B = torch.tensor(1.0, requires_grad=True).float()
+lr = 0.001
+
+def pred(x): return W * X + B
+def mse(Yp, Y): return ((Yp - Y)**2).mean()
+
+Yp = pred(X)
+loss = mse(Yp,Y)
+loss.backward()
+
+with torch.no_grad():
+    W -= lr*W.grad
+    B -= lr*B.grad
+
+W.grad.zero_()
+B.grad.zero_()
